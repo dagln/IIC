e6f3a84ecd7d0c7c7067dd0dbadd69a55015cacc
dagln <43912872+dagln@users.noreply.github.com>
2018-10-09 19:10:10 +0300

    general remarks

--- ITS/ClientArea/src/Components/TreeView/TreeView.component.ts
+++ ITS/ClientArea/src/Components/TreeView/TreeView.component.ts
@@ -10,10 +10,15 @@ export class TreeViewComponent implements OnInit {
 @ContentChild(TemplateRef) _template: TemplateRef<ElementRef>;
 @ContentChildren(TemplateRef) _templates: QueryList<TemplateRef<ElementRef>>;
 
-_list: Number[] = [1,2,3];
+_list: Number[] = [1, 2, 3];
   constructor() { }
 
   ngOnInit() {
+    // contentchildren the templates by using the new directive as selector, not TemplateRef. 
+    // - this will give you a way to identify each template.
+    
+    // use the id provided in the new directive to retreive a template from the querylist 
+    // and use that template in an ngTemplateOutlet in an ngTemplate tag or an ngContainer.
   }
 
 }
