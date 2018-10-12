c1fb5cc5eac0a4e6ccd9f55f3ecca460b70f28a0
dagln <43912872+dagln@users.noreply.github.com>
2018-10-12 14:19:16 +0300

    Revert "template progress#1"
    This reverts commit 74d2e3cecda0f71bc6ca52c38faba5f318428cfd.

--- ITS/ClientArea/src/Components/TreeView/TreeView.component.ts
+++ ITS/ClientArea/src/Components/TreeView/TreeView.component.ts
@@ -9,29 +9,11 @@ import { ContentChildren, Component, OnInit, Input, ViewChildren,
 export class TreeViewComponent implements OnInit {
 @ContentChild(TemplateRef) _template: TemplateRef<ElementRef>;
 @ContentChildren(TemplateRef) _templates: QueryList<TemplateRef<ElementRef>>;
-_temps: TemplateRef<ElementRef>[];
-ctx = {name: "bla"}
 
-_list: number[] = [1,2,3];
-_ilist: Thing[] = [new Thing("right", 1), new Thing("now", 2)];
-  constructor() {
-  }
+_list: Number[] = [1,2,3];
+  constructor() { }
 
-  getText(): string {
-    return "fine, just fine";
-  }
   ngOnInit() {
-   //this._temps = this._templates.toArray();
   }
 
-}
-
-export class Thing{
-  public name: string;
-  public counter: number
-
-  constructor(name: string, counter: number) {
-    this.name = name;
-    this.counter = counter;
-  }
 }
