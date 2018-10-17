aa82a06fe8f58841d9bd401dffd0f8550b3f0e58
dagln <43912872+dagln@users.noreply.github.com>
2018-10-17 13:58:26 +0300

    variables

--- ITS/ClientArea/src/Components/TreeView/TreeView.component.ts
+++ ITS/ClientArea/src/Components/TreeView/TreeView.component.ts
@@ -1,5 +1,5 @@
-import { ContentChildren, Component, OnInit, Input, ViewChildren, 
-        QueryList, ContentChild, TemplateRef, ElementRef } from '@angular/core';
+import { ContentChildren, Component, OnInit, Input, ViewChildren,
+  QueryList, ContentChild, TemplateRef, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'TreeView',
@@ -10,7 +10,7 @@ export class TreeViewComponent implements OnInit {
 @ContentChild(TemplateRef) _template: TemplateRef<ElementRef>;
 @ContentChildren(TemplateRef) _templates: QueryList<TemplateRef<ElementRef>>;
 _temps: TemplateRef<ElementRef>[];
-ctx = {name: 'bla'}
+ctx = {name: 'bla'};
 
 _list: number[] = [1, 2, 3];
 _ilist: Thing[] = [new Thing('right', 1), new Thing('now', 2)];
@@ -26,7 +26,7 @@ _ilist: Thing[] = [new Thing('right', 1), new Thing('now', 2)];
 
 }
 
-export class Thing{
+export class Thing {
   public name: string;
   public counter: number;
 
