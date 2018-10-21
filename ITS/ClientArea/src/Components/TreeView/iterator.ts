7c56994446b91cb5ee8be36c6e732fc0639ce219
dagln <43912872+dagln@users.noreply.github.com>
2018-10-21 19:43:17 +0300

    itereator interface

---
+++ ITS/ClientArea/src/Components/TreeView/iterator.ts
@@ -0,0 +1,40 @@
+import { TemplateRef } from "@angular/core";
+
+export interface Iterator<Item> {
+       next: () => void;
+       current: () => Item;
+       is_done: () => boolean;
+}
+class ArrayIterator<T> implements Iterator<T> {
+    items : T[];
+    _current : number;
+
+    constructor(items : T[]) {
+        this.items = items;
+    }
+    next(): void {
+        this._current++;
+    }
+    current(): T {
+        return this.items[this._current];
+    }
+    is_done(): boolean {
+        return this._current == this.items.length - 1;
+    }
+}
+
+
+ export interface AggregateIterationBuilder<T> {
+     create_iterator() : Iterator<T>;
+ }
+ export class ArrayIterationBuilder<T> implements AggregateIterationBuilder<T> {
+     items: T[];
+     constructor(items: T[]) {
+         this.items = items;
+     }
+     create_iterator(): ArrayIterator<T> {
+        return new ArrayIterator<T>(this.items);
+     }
+ }
+
+ 
